8500b924e366466e8a9aa8e85f0f08fd27cd2e6c
viktorovchinnikov <viktor.ovchinnikov@yadro.com>
2025-03-24 16:04:25 +0000

    Lesson 7 MCU Course ADC homework starting point

--- app/main.c
+++ app/main.c
@@ -1,34 +1,50 @@
+#include "mik32_hal_adc.h"
 #include "mik32_hal_usart.h"
 #include "mik32_hal_scr1_timer.h"
+#include "xprintf.h"
 
-/*
- * Данный пример демонстрирует вывод данных через USART.
- */
+#define ADC_CHANNEL_TOTAL                6U
+#define ADC_CONVERSIONS_PER_CHANNEL      2U
+#define ADC_RAW_VALUE_MAX                4095U
+#define ADC_REF_VOLTAGE_MV               1200U /* mV */
+#define MILLIVOLTS_PER_VOLT              1000U
+#define LOOP_DELAY_MS                    800U
 
 
 static void SystemClock_Config();
 static void USART_Init();
-static uint32_t fibonachi(uint32_t n);
 
 static USART_HandleTypeDef husart0;
+ADC_HandleTypeDef hadc;
+
+static void ADC_Init(void);
 
 int main()
 {
-    // volatile только чтобы оставить unused переменные для отладки
-    volatile uint32_t fibonachi_result;
-    volatile uint64_t start_mtime, end_mtime;
+    uint16_t adc_value = 0U;
+    uint32_t integer_part = 0U;
+    uint32_t fractional_part = 0U;
     SystemClock_Config();
 
     USART_Init();
+    ADC_Init();
 
     while (1)
     {
         HAL_USART_Print(&husart0, "YADRO RISCV\r\n", USART_TIMEOUT_DEFAULT);
-        start_mtime = __HAL_SCR1_TIMER_GET_TIME();
-        fibonachi_result = fibonachi(30);
-        end_mtime = __HAL_SCR1_TIMER_GET_TIME();
-        // здесь можно вывести время выполнения функции
-        HAL_DelayMs(5000);
+        for (uint32_t channel = 0U; channel < ADC_CHANNEL_TOTAL; channel++) {
+            for (uint32_t conversion = 0U; conversion < ADC_CONVERSIONS_PER_CHANNEL; conversion++) {
+                HAL_ADC_SINGLE_AND_SET_CH(hadc.Instance, channel);
+                adc_value = HAL_ADC_WaitAndGetValue(&hadc);
+                if (0U == conversion) {
+                    continue;
+                }
+                integer_part = adc_value * ADC_REF_VOLTAGE_MV / ADC_RAW_VALUE_MAX / MILLIVOLTS_PER_VOLT;
+                fractional_part = adc_value * ADC_REF_VOLTAGE_MV / ADC_RAW_VALUE_MAX % MILLIVOLTS_PER_VOLT;
+                xprintf("ADC[%d]: %04d/%d (%d,%03d V)\r\n", channel, adc_value, ADC_RAW_VALUE_MAX, integer_part, fractional_part);
+            }
+        }
+        HAL_DelayMs(LOOP_DELAY_MS);
     }
 }
 
@@ -48,6 +64,27 @@ void SystemClock_Config(void)
     PCC_OscInit.RTCClockSelection = PCC_RTC_CLOCK_SOURCE_AUTO;
     PCC_OscInit.RTCClockCPUSelection = PCC_CPU_RTC_CLOCK_SOURCE_OSC32K;
     HAL_PCC_Config(&PCC_OscInit);
+}
+
+static void ADC_Init(void) {
+    hadc.Instance = ANALOG_REG;
+    hadc.Init.EXTRef = ADC_EXTREF_OFF;
+    hadc.Init.EXTClb = ADC_EXTCLB_CLBREF;
+
+    HAL_ADC_Init(&hadc);
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    __HAL_PCC_ANALOG_REGS_CLK_ENABLE();
+
+    GPIO_InitStruct.Mode = HAL_GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = HAL_GPIO_PULL_NONE;
+    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_7;
+    HAL_GPIO_Init(GPIO_1, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_4 | GPIO_PIN_7 | GPIO_PIN_9;
+    HAL_GPIO_Init(GPIO_0, &GPIO_InitStruct);
 }
 
 void USART_Init()
@@ -89,19 +126,6 @@ void USART_Init()
     husart0.Modem.dsr = Disable; //in
     husart0.Modem.ri = Disable;  //in
     husart0.Modem.ddis = Disable;//out
-    husart0.baudrate = 9600;
+    husart0.baudrate = 115200;
     HAL_USART_Init(&husart0);
-}
-
-uint32_t fibonachi(uint32_t n)
-{
-    if (n == 0)
-    {
-        return 0;
-    }
-    if ((n == 1) || (n == 2))
-    {
-        return 1;
-    }
-    return fibonachi(n - 1) + fibonachi(n - 2);
 }
